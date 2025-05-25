e8f2c7407689532f49d6b65a9de046a475f13210
serhat <serhat1696@outlook.com>
2025-05-25 22:54:09 +0300

    Ignore ESLint and TypeScript errors during build

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // You can keep other config options here if needed
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
