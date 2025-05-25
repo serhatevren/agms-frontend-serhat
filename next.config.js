e477d8455a135169251d850329ec1525b46548e4
serhat <serhat1696@outlook.com>
2025-05-25 22:43:28 +0300

    Switch to JS config and disable ESLint for Vercel

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // You can keep other config options here if needed
+};
+
+module.exports = nextConfig;
